a7558706b486765d66e0fd99f679cce8cf8c2955
maur025 <mmoyapalli@gmail.com>
2026-09-02 12:17:51 -0400

    before upload

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,17 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'node:path';
 import { CommonModule } from './common/common.module.js';
+import { EnvConfiguration } from './config/app.config.js';
+import { JoiValidationSchema } from './config/joi.validation.js';
 import { PokemonModule } from './pokemon/pokemon.module.js';
 import { SeedModule } from './seed/seed.module.js';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: JoiValidationSchema,
+      load: [EnvConfiguration],
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(import.meta.dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('mongodb'),
+        dbName: 'pokemonsdb',
+      }),
+    }),
 
     CommonModule,
 
